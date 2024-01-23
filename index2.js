220de82ad8c8f719eec331179040f7a25a77528f
EliseoMaci <leo.maci2006@gmail.com>
2024-01-23 20:30:54 +0100

    index2.js

--- index2.js
+++ index2.js
@@ -2,50 +2,46 @@ const photoContainer = document.getElementById("photoContainer");
 const toggleSwitch = document.getElementById("toggleSwitch");
 const loadMoreBtn = document.getElementById("loadMoreBtn");
 
-async function fetchPhotos() {
-    console.log("Fetching photos...");
-    photoContainer.innerHTML = "";
-    try {
-        for (let i = 0; i < 4; i++) {
-            const response = await fetch("https://picsum.photos/375/375");
-            if (!response.ok) {
-                throw new Error("Failed to fetch photo");
-            }
+function fetchPhotos() {
+  photoContainer.innerHTML = "";
 
-            const photoUrl = response.url;
-            const img = document.createElement("img");
-            img.src = photoUrl;
-            img.alt = "Random Photo";
-
-            photoContainer.appendChild(img);
+  for (let i = 0; i < 4; i++) {
+    fetch("https://picsum.photos/375/375")
+      .then((response) => {
+        if (response.ok) {
+          return response.url;
+        } else {
+          throw new Error("Failed to fetch photo");
         }
-        console.log("Photos fetched.");
-    } catch (error) {
-        console.error(error);
-    }
+      })
+      .then((photoUrl) => {
+        // Create an image element
+        const img = document.createElement("img");
+        img.src = photoUrl;
+        img.alt = "Random Photo";
+
+        // Append the image to the container
+        photoContainer.appendChild(img);
+      })
+      .catch((error) => console.error(error));
+  }
 }
 
 function applyGreyscale() {
-    console.log("Applying greyscale...");
-    const images = photoContainer.querySelectorAll("img");
-    images.forEach((img) => {
-        if (toggleSwitch.checked) {
-            img.classList.add("greyscale");
-        } else {
-            img.classList.remove("greyscale");
-        }
-    });
-    console.log("Greyscale applied.");
+  const images = photoContainer.querySelectorAll("img");
+  images.forEach((img) => {
+    if (toggleSwitch.checked) {
+      img.classList.add("greyscale");
+    } else {
+      img.classList.remove("greyscale");
+    }
+  });
 }
 
 fetchPhotos();
+
 toggleSwitch.addEventListener("change", function () {
-    console.log("Toggle switch changed.");
-    applyGreyscale();
+  applyGreyscale();
 });
 
-loadMoreBtn.addEventListener("click", function (event) {
-    event.preventDefault();
-    console.log("Load more button clicked.");
-    fetchPhotos();
-});
+loadMoreBtn.addEventListener("click", fetchPhotos);
