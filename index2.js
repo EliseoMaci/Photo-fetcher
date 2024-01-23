53c91048f7845adabd21d74e2c154d5ea51780a8
EliseoMaci <leo.maci2006@gmail.com>
2024-01-23 20:33:26 +0100

    index2.js

--- index2.js
+++ index2.js
@@ -1,12 +1,13 @@
 const photoContainer = document.getElementById("photoContainer");
 const toggleSwitch = document.getElementById("toggleSwitch");
 const loadMoreBtn = document.getElementById("loadMoreBtn");
-
 function fetchPhotos() {
+  // Clear the existing content of the photo container
   photoContainer.innerHTML = "";
 
+  // Fetch 4 random photos from the API
   for (let i = 0; i < 4; i++) {
-    fetch("https://picsum.photos/375/375")
+    fetch("https://picsum.photos/367/367")
       .then((response) => {
         if (response.ok) {
           return response.url;
@@ -22,12 +23,17 @@ function fetchPhotos() {
 
         // Append the image to the container
         photoContainer.appendChild(img);
+
+        // const photoInfo = document.createElement("p");
+        // photoInfo.textContent = `Author: ${info.author}`;
+        // photoContainer.appendChild(photoInfo);
       })
       .catch((error) => console.error(error));
   }
 }
 
-function applyGreyscale() {
+function applyGrayscale() {
+  // Apply grayscale class to all images if the toggle switch is enabled
   const images = photoContainer.querySelectorAll("img");
   images.forEach((img) => {
     if (toggleSwitch.checked) {
@@ -38,10 +44,11 @@ function applyGreyscale() {
   });
 }
 
+function loadMore() {}
+
+// Initial load of photos
 fetchPhotos();
 
-toggleSwitch.addEventListener("change", function () {
-  applyGreyscale();
-});
-
+// Apply grayscale effect when the switch is changed
+toggleSwitch.addEventListener("click", applyGrayscale);
 loadMoreBtn.addEventListener("click", fetchPhotos);
